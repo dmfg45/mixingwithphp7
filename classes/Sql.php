6769108bb3690e1b4f1f1660dedb77a74677f757
André Graça <andre.graca.45@gmail.com>
2019-12-05 02:48:21 +0000

    updating project

--- classes/Sql.php
+++ classes/Sql.php
@@ -6,7 +6,7 @@ class Sql extends PDO
 
     public function __construct()
     {
-        $this->connection = new PDO('mysql:host=localhost;dbname=php7db', 'root', 'Server45Server7AGCD');
+        $this->connection = new PDO('mysql:host=localhost;dbname=php7db', 'root', 'root');
     }
 
     private function setParams($statement, $params = array())
